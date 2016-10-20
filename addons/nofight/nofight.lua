6ae5e6105a43be26dd585303de49ce371bfee801
Nasiko <nasiko.tw@gmail.com>
2016-10-21 03:17:03 +0800

    Update nofight.lua

--- addons/nofight/nofight.lua
+++ addons/nofight/nofight.lua
@@ -3,9 +3,12 @@
 local acutil = require("acutil");
 
 function NOFIGHT_ON_INIT(addon, frame)
+	--取代 community.lua 的 ASKED_FRIENDLY_FIGHT
 	acutil.setupHook(ASKED_FRIENDLY_FIGHT_HOOKED, "ASKED_FRIENDLY_FIGHT");
 end
 
 function ASKED_FRIENDLY_FIGHT_HOOKED(handle, familyName)
-  CHAT_SYSTEM("{@st41}您已經無視了" .. familyName .. "的" .. ScpArgMsg("RequestFriendlyFight"));
+	-- 忽略請求，不做任何回應，並且通知使用者
+	CHAT_SYSTEM("{@st41}您已經忽略了" .. familyName .. "的" .. ScpArgMsg("RequestFriendlyFight"));
+	
 end
